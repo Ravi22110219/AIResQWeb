af0d96407afb4bc985ac749c45127f2bf685708a
Ravi Kumawat <ravi.kumawat@iitgn.ac.in>
2026-03-08 12:35:05 +0530

    vite updated

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FloodAIRepo/',   // IMPORTANT for GitHub Pages
+  base: '/',   // Correct for custom domain deployment
   build: {
     outDir: 'dist',
   },
